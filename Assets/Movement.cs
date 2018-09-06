d77549ccd1b2e73b447d468eb12c7253270359bb
Cody Cook <codycook33@gmail.com>
2018-09-06 11:47:17 -0700

    Added everything to folder

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed;
+
+	public float jumpPower;
+	// Use this for initialization
+	void Start ()
+	{
+		speed = 5f;
+		jumpPower = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal")* speed * Time.deltaTime, Input.GetAxis("Jump") * jumpPower * Time.deltaTime, 0f);
+	}
+}
